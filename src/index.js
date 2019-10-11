1a4318edd45bb3425ae95fbe07d99ae3a6cb25db
romasr <shaman.roman@tut.by>
2019-10-11 21:38:47 +0300

    feat: add function for Zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function zeros(expression) {
-  // your solution
+module.exports = function getZerosCount(expression) {
+
+  let multiplier = { '2': 0, '5': 0 };
+
+  expression.split('*').forEach(fact => {
+
+		let isDouble = fact.match(/!/g).length == 2 ? true : false;
+		fact = fact.substring(0, fact.indexOf('!'))
+		let factorialParity = fact % 2 == 0 ? true : false;
+
+		Object.keys(multiplier).forEach(m => {
+			if (isDouble && !factorialParity && m % 2 == 0) return;
+			let limit = 1;
+			do {
+				limit *= m;
+				let count = Math.floor(fact / limit);
+				if (isDouble && m % 2 != 0) {
+					if (factorialParity) count = Math.floor(count / 2);
+					else count = Math.ceil(count / 2);
+				}
+				multiplier[m] += count;
+			} while (limit <= fact)
+		})
+	})
+	return Math.min(multiplier['2'], multiplier['5'])
 }
